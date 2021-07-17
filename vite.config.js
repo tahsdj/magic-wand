9642d59e6208f461998b8b477cb2b4543c368c35
paultsai <tahsdj@gmail.com>
2021-07-17 15:01:01 +0800

    add deploy code

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://tahsdj.github.io/magic-wand/',
   plugins: [reactRefresh(), WindiCSS()]
 })
